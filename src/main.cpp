fa385244a85ab8a0f416157adbcf8a1178d0750b
Scalaptia <fernando@haroware.com>
2024-05-25 20:01:29 -0700

    Add game thread

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,68 @@ using std::endl;
 using std::string;
 using std::thread;
 
+void Game()
+{
+    // Juego
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    // SetTraceLogLevel(LOG_NONE); // Disable raylib logging
+    InitWindow(screenWidth, screenHeight, "Espyntar");
+
+    ColorPalette palette;
+    Canvas canvas(screenWidth, screenHeight, palette);
+    Painter painter(palette, canvas);
+
+    SetTargetFPS(144);
+
+    while (!WindowShouldClose())
+    {
+        Vector2 position = GetMousePosition();
+
+        float mouseWheelMove = GetMouseWheelMove();
+        painter.SetBrushSize(mouseWheelMove);
+
+        // Paint if the left mouse button is pressed
+        if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+        {
+            // Check if a color in the palette is clicked
+            int colorIndex = canvas.CheckPaletteClick(palette);
+            if (colorIndex >= 0)
+            {
+                painter.SetColor(colorIndex);
+            }
+            else
+            {
+                painter.Paint(position);
+            }
+        }
+        else
+        {
+            painter.ResetLastPosition();
+        }
+
+        // Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Canvas
+        Rectangle rec = {0.0f, 0.0f, (float)canvas.GetTarget().texture.width, (float)-canvas.GetTarget().texture.height};
+        Vector2 vec = {0.0f, 0.0f};
+        DrawTextureRec(canvas.GetTarget().texture, rec, vec, WHITE);
+
+        // Brush
+        DrawCircleLines(GetMouseX(), GetMouseY(), painter.GetBrushSize(), painter.GetColor());
+
+        // Palette
+        canvas.DrawPalette(palette);
+        DrawFPS(GetScreenWidth() - 95, 10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
+
 int main(void)
 {
     if (!InitWinsock())
@@ -44,65 +106,11 @@ int main(void)
     thread receiverThread([&client]()
                           { client.Receive(); });
 
+    thread juegoThread(Game);
+
+    juegoThread.join();
     senderThread.join();
     receiverThread.join();
-
-    // // Juego
-    // const int screenWidth = 800;
-    // const int screenHeight = 450;
-
-    // InitWindow(screenWidth, screenHeight, "Espyntar");
-
-    // ColorPalette palette;
-    // Canvas canvas(screenWidth, screenHeight, palette);
-    // Painter painter(palette, canvas);
-
-    // SetTargetFPS(144);
-
-    // while (!WindowShouldClose())
-    // {
-    //     Vector2 position = GetMousePosition();
-
-    //     float mouseWheelMove = GetMouseWheelMove();
-    //     painter.SetBrushSize(mouseWheelMove);
-
-    //     // Paint if the left mouse button is pressed
-    //     if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
-    //     {
-    //         // Check if a color in the palette is clicked
-    //         int colorIndex = canvas.CheckPaletteClick(palette);
-    //         if (colorIndex >= 0)
-    //         {
-    //             painter.SetColor(colorIndex);
-    //         }
-    //         else
-    //         {
-    //             painter.Paint(position);
-    //         }
-    //     }
-    //     else
-    //     {
-    //         painter.ResetLastPosition();
-    //     }
-
-    //     // Draw
-    //     BeginDrawing();
-    //     ClearBackground(RAYWHITE);
-
-    //     // Canvas
-    //     Rectangle rec = {0.0f, 0.0f, (float)canvas.GetTarget().texture.width, (float)-canvas.GetTarget().texture.height};
-    //     Vector2 vec = {0.0f, 0.0f};
-    //     DrawTextureRec(canvas.GetTarget().texture, rec, vec, WHITE);
-
-    //     // Brush
-    //     DrawCircleLines(GetMouseX(), GetMouseY(), painter.GetBrushSize(), painter.GetColor());
-
-    //     // Palette
-    //     canvas.DrawPalette(palette);
-    //     DrawFPS(GetScreenWidth() - 95, 10);
-    //     EndDrawing();
-    // }
-    // CloseWindow();
 
     return 0;
 }
