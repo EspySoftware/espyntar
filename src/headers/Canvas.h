443f13e04ee93b10c553a2c5f9db01374f28b956
Scalaptia <fernando@haroware.com>
2024-06-06 16:58:37 -0700

    Merge rama Miti (canva no funciona)

--- src/headers/Canvas.h
+++ src/headers/Canvas.h
@@ -1,6 +1,8 @@
 #pragma once
-#include <raylib.h>
+
 #include "ColorPalette.h"
+#include <queue>
+#include <raylib.h>
 
 #define MAX_COLORS_COUNT 23
 
@@ -8,6 +10,11 @@ class Canvas
 {
 private:
     RenderTexture2D target;
+    Color GetColorAt(int x, int y);    // Get color from RenderTexture2D at position
+    bool IsWithinBounds(int x, int y); // Check if position is within RenderTexture2D bounds
+    void DrawPixel(int x, int y, Color color);
+    int width;
+    int height;
 
 public:
     Canvas(int width, int height, ColorPalette &palette);
@@ -16,6 +23,7 @@ public:
     void Draw(Vector2 position, float radius, Color color);
     void DrawPalette(ColorPalette &palette);
     int CheckPaletteClick(ColorPalette &palette);
+    void BucketFill(Vector2 position, Color newColor);
 
     RenderTexture2D GetTarget() { return target; }
 };
--- src/headers/Game.h
+++ src/headers/Game.h
@@ -1,11 +1,86 @@
 #pragma once
-#include "./Inicio.h"
+
+// Tools
+enum Tool
+{
+    BRUSH,
+    BUCKET
+};
 
 void drawGame(Screen *screen)
 {
+    static bool initialized = false;
+    static ColorPalette *palette;
+    static Canvas *canvas;
+    static Painter *painter;
+    static Tool currentTool = BRUSH;
+
+    if (!initialized)
+    {
+        palette = new ColorPalette();
+        canvas = new Canvas(800, 450, *palette);
+        painter = new Painter(*palette, *canvas);
+        initialized = true;
+    }
+
+    Vector2 position = GetMousePosition();
+
+    float mouseWheelMove = GetMouseWheelMove();
+    painter->SetBrushSize(mouseWheelMove);
+
+    // Switch tool with SPACE key
+    if (IsKeyPressed(KEY_SPACE))
+    {
+        currentTool = (currentTool == BRUSH) ? BUCKET : BRUSH;
+    }
+
+    // Paint or fill if the left mouse button is pressed
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+    {
+        // Check if a color in the palette is clicked
+        int colorIndex = canvas->CheckPaletteClick(*palette);
+        if (colorIndex >= 0)
+        {
+            painter->SetColor(colorIndex);
+        }
+        else
+        {
+            if (currentTool == BRUSH)
+            {
+                painter->Paint(position);
+            }
+            else if (currentTool == BUCKET)
+            {
+                painter->Fill(position);
+            }
+        }
+    }
+    else
+    {
+        painter->ResetLastPosition();
+    }
+
+    // Draw
     BeginDrawing();
-    ClearBackground(BLACK);
+    ClearBackground(RAYWHITE);
     DrawTexture(screen->background, 0, 0, WHITE);
 
+    // Canvas
+    Rectangle rec = {screenWidth / 2.0f - 425.0f, screenHeight / 2.0f - 225.0f, (float)canvas->GetTarget().texture.width, (float)-canvas->GetTarget().texture.height};
+    Vector2 vec = {screenWidth / 2.0f - 425.0f, screenHeight / 2.0f - 225.0f};
+    DrawTextureRec(canvas->GetTarget().texture, rec, vec, WHITE);
+
+    // Brush outline
+    if (currentTool == BRUSH)
+    {
+        DrawCircleLines(GetMouseX(), GetMouseY(), painter->GetBrushSize(), painter->GetColor());
+    }
+    else if (currentTool == BUCKET)
+    {
+        DrawCircleLines(GetMouseX(), GetMouseY(), 10, painter->GetColor()); // Small circle for bucket tool indicator
+    }
+
+    // Palette
+    canvas->DrawPalette(*palette);
     EndDrawing();
 }
