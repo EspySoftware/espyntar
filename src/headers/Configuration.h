34347ae1ab0c6981bc010a155a6d979ae5e6e295
Daniel Solano <solanomezaangel@gmail.com>
2024-06-08 16:44:47 -0700

    Revert "Merge branch 'client' into Miti"
    This reverts commit d14dadbbba576e8b8718bcc2194d9fd11efa70d7, reversing
    changes made to 96c69bdbc1cbbf8538c9b04aac5d7d06649ddff9.

--- src/headers/Configuration.h
+++ src/headers/Configuration.h
@@ -32,72 +32,50 @@ inline void Configuration::drawConfig(Screen *scene)
 {
     BeginDrawing();
 
+
     DrawTexture(this->background, 0, 0, WHITE);
     DrawTexture(this->title, (GetScreenWidth() - this->title.width) / 2, 50, WHITE);
 
-    Color color1 = {102, 149, 89, 200};
-    Rectangle rec = {
-        (GetScreenWidth() / 2.0f) - 175.0f,
-        (GetScreenHeight() / 2.0f) - 150.0f,
-        350.0f,
-        400.0f};
-    DrawRectangleRounded(rec, 0.5f, 0.5, color1);
-
-    // Posición central del rectángulo
-    float rectCenterX = rec.x + (rec.width / 2.0f);
-    float rectCenterY = rec.y + (rec.height / 2.0f);
-    float buttonWidth = 170.0f;
-    float buttonHeight = 50.0f;
-    float spacing = 20.0f; // Espacio entre botones
-
-    // Calcular posiciones Y de los botones de manera que estén centrados
-    float totalHeight = (buttonHeight * 4) + (spacing * 3);
-    float startY = rectCenterY - (totalHeight / 2.0f);
-
-    // Botón de Música
-    buttons(rectCenterX - (buttonWidth / 2.0f), startY, buttonWidth, buttonHeight, "MUSICA");
+    buttons(GetScreenWidth() / 2.0f - 60, GetScreenHeight() / 2.0f + 90.0f, 160.0f, 50.0f, "MUSICA");
     if (this->musicOn) // musica activada
     {
-        if (GuiButton({rectCenterX - (buttonWidth / 2.0f), startY, buttonWidth, buttonHeight}, "MUSICA on"))
+        if (GuiButton({(GetScreenWidth() / 2.0f) - 60, GetScreenHeight() / 2.0f + 90.0f, 160.0f, 50.0f}, "MUSICA on"))
         {
             this->musicOn = false; // desactivar musica
         }
     }
     else // musica desactivada
     {
-        if (GuiButton({rectCenterX - (buttonWidth / 2.0f), startY, buttonWidth, buttonHeight}, "MUSICA off"))
+        if (GuiButton({(GetScreenWidth() / 2.0f) - 60, GetScreenHeight() / 2.0f + 90.0f, 160.0f, 50.0f}, "MUSICA off"))
         {
             this->musicOn = true; // activar musica
         }
     }
 
-    // Botón de Efectos
-    buttons(rectCenterX - (buttonWidth / 2.0f), startY + buttonHeight + spacing, buttonWidth, buttonHeight, "EFECTOS");
+    buttons(GetScreenWidth() / 2.0f - 60, GetScreenHeight() / 2.0f + 20.0f, 160.0f, 50.0f, "EFECTOS");
     if (this->effects) // efectos activados
     {
-        if (GuiButton({rectCenterX - (buttonWidth / 2.0f), startY + buttonHeight + spacing, buttonWidth, buttonHeight}, "EFECTOS on"))
+        if (GuiButton({(GetScreenWidth() / 2.0f) - 60, GetScreenHeight() / 2.0f + 20.0f, 160.0f, 50.0f}, "EFECTOS on"))
         {
             this->effects = false; // desactivar efectos
         }
     }
     else // efectos desactivados
     {
-        if (GuiButton({rectCenterX - (buttonWidth / 2.0f), startY + buttonHeight + spacing, buttonWidth, buttonHeight}, "EFECTOS off"))
+        if (GuiButton({(GetScreenWidth() / 2.0f) - 60, GetScreenHeight() / 2.0f + 20.0f, 160.0f, 50.0f}, "EFECTOS off"))
         {
             this->effects = true; // activar efectos
         }
     }
 
-    // Botón Volver
-    buttons(rectCenterX - (buttonWidth / 2.0f), startY + 2 * (buttonHeight + spacing), buttonWidth, buttonHeight, "VOLVER");
-    if (GuiButton({rectCenterX - (buttonWidth / 2.0f), startY + 2 * (buttonHeight + spacing), buttonWidth, buttonHeight}, "VOLVER"))
+    buttons(GetScreenWidth() / 2.0f - 60, GetScreenHeight() / 2.0f + 160.0f, 160.0f, 50.0f, "VOLVER");
+    if (GuiButton({(GetScreenWidth() / 2.0f) - 60, GetScreenHeight() / 2.0f + 160.0f, 160.0f, 50.0f}, "VOLVER"))
     {
         scene->scene = GAME;
     }
 
-    // Botón Salir
-    buttons(rectCenterX - (buttonWidth / 2.0f), startY + 3 * (buttonHeight + spacing), buttonWidth, buttonHeight, "SALIR");
-    if (GuiButton({rectCenterX - (buttonWidth / 2.0f), startY + 3 * (buttonHeight + spacing), buttonWidth, buttonHeight}, "SALIR"))
+    buttons(GetScreenWidth() / 2.0f - 60, GetScreenHeight() / 2.0f + 190.0f, 160.0f, 50.0f, "SALIR");
+    if (GuiButton({(GetScreenWidth() / 2.0f) - 60, GetScreenHeight() / 2.0f + 190.0f, 160.0f, 50.0f}, "SALIR"))
     {
         scene->scene = EXIT;
     }
